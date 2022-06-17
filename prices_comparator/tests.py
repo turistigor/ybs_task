05ff494d8010b3657de48acdace6c345029bf9e5
igor <igor_m_87@mail.ru>
2022-06-17 19:24:34 +0300

    imports post updateData validation and first test

--- prices_comparator/tests.py
+++ prices_comparator/tests.py
@@ -1,20 +1,45 @@
 from django.test import TestCase
 
 import requests as http
-import uuid
+import json
 
 
-class PriceComparatorTest(TestCase):
-    def test_loop(self):
-        data = {
-            'id': uuid.uuid1(), 
-            'name': 'soft',
-            # 'parentId': None
-            'type': 'CATEGORY',
-            'price': 0
+class ImportTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.normal_item = {
+            'id': '3fa85f64-5717-4562-b3fc-2c963f66a444',
+            'name': 'Оффер',
+            'parentId': '3fa85f64-5717-4562-b3fc-2c963f66a333',
+            'price': 234,
+            'type': 'OFFER'
         }
 
-        url = 'http://127.0.0.1:8000/imports'
-        resp = http.post(url=url, data=data)
+        cls._validation_failed = {
+            "code": 400,
+            "message": "Validation Failed"
+        }
+
+        cls._imports_url = 'http://127.0.0.1:8000/imports'
+
+    def _send(self, data):
+        return http.post(url=self._imports_url, 
+                         data=json.dumps(data))
+
+    def test_updateDate(self):
+        # request without field
+        data = {'items':[self.normal_item]}
+        resp = self._send(data)
+
+        self.assertEqual(resp.status_code, 400)
+        self.assertDictEqual(
+            json.loads(resp.content.decode()), self._validation_failed
+        )
+
+        # from example: "2022-05-28T21:12:01.000Z"
+        # incorrect: "cwlef,l"
+        # empty: ""
+        # '2006-10-25 14:30'
+        # '2006-10-25T14:30+02:00'
 
 
--- prices_comparator/views.py
+++ prices_comparator/views.py
@@ -1,9 +1,24 @@
-from django.shortcuts import render
 from django.views import View
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseBadRequest
+import django.forms as forms
+
+import json 
+
+
+class ImportForm(forms.Form):
+    # items = forms.CharField()
+    updateDate = forms.DateTimeField()
 
 
 class PricesComparatorView(View):
     def post(self, request):
-        return HttpResponse(request.POST)
-
+        data = json.loads(request.body.decode())
+        import_form = ImportForm(data)
+        if import_form.is_valid():
+            #TODO: create db models, validate and save
+            return HttpResponse()
+        else:
+            return HttpResponseBadRequest(json.dumps({
+                "code": 400,
+                "message": "Validation Failed"
+            }))
