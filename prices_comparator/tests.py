813e6ecf54cf4cb553c9a4e7d06d2a489c491624
igor <igor_m_87@mail.ru>
2022-06-17 15:16:41 +0300

    tests added

--- prices_comparator/tests.py
+++ prices_comparator/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
 
-# Create your tests here.
+import requests as http
+import uuid
+
+
+class PriceComparatorTest(TestCase):
+    def test_loop(self):
+        data = {
+            'id': uuid.uuid1(), 
+            'name': 'soft',
+            # 'parentId': None
+            'type': 'CATEGORY',
+            'price': 0
+        }
+
+        url = 'http://127.0.0.1:8000/imports'
+        resp = http.post(url=url, data=data)
+
+
