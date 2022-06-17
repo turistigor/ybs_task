887884b3a92f59106b680750f98ca5406026786f
igor <igor_m_87@mail.ru>
2022-06-18 01:28:23 +0300

    items field for import validation form added

--- prices_comparator/views.py
+++ prices_comparator/views.py
@@ -1,12 +1,59 @@
 from django.views import View
 from django.http import HttpResponse, HttpResponseBadRequest
 import django.forms as forms
+from django.core.exceptions import ValidationError
 
-import json 
+import json
+
+
+class ImportItemForm(forms.Form):
+    id = forms.UUIDField()
+    name = forms.CharField()
+    parentId = forms.UUIDField()
+    type = forms.ChoiceField(
+        choices=(('OFFER', 'OFFER'), ('CATEGORY', 'CATEGORY'))
+    )
+    price = forms.IntegerField(min_value=0)
+
+
+class ListField(forms.MultipleChoiceField):
+    def __init__(self, *args, item_form_class=None, **kwargs):
+        self.form_class = item_form_class
+        super().__init__()
+
+    def to_python(self, values):
+        if not values:
+            return []
+        elif not isinstance(values, (list, tuple)):
+            raise ValidationError(
+                self.error_messages["invalid_list"], code="invalid_list"
+            )
+        return values
+
+    def validate(self, value):
+        if self.required and not value:
+            raise ValidationError(self.error_messages["required"], code="required")
+
+        ids = set()
+
+        for val in value:
+            form = self.form_class(val)
+            if not form.is_valid():
+                raise ValidationError(
+                    self.error_messages["invalid_choice"],
+                    code="invalid_choice",
+                    params={"value": val},
+                )
+            else:
+                new_id = form.cleaned_data['id']
+                if new_id in ids:
+                    raise ValidationError('id is not unique in the imported set')
+                else:
+                    ids.add(new_id)
 
 
 class ImportForm(forms.Form):
-    # items = forms.CharField()
+    items = ListField(item_form_class=ImportItemForm)
     updateDate = forms.DateTimeField()
 
 
@@ -14,6 +61,7 @@ class PricesComparatorView(View):
     def post(self, request):
         data = json.loads(request.body.decode())
         import_form = ImportForm(data)
+
         if import_form.is_valid():
             #TODO: create db models, validate and save
             return HttpResponse()
