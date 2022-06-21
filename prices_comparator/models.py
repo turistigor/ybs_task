295e2ecb741d2bfe146d0148417d9bf4cb4ffa01
igor <igor_m_87@mail.ru>
2022-06-21 18:20:48 +0300

    imports model fixed

--- prices_comparator/models.py
+++ prices_comparator/models.py
@@ -8,5 +8,8 @@ class ImportModel(models.Model):
     name = models.CharField(max_length=const.IMPORT_UNIT_NAME_MAX_LENGTH)
     date = models.DateTimeField()
     parent_id = models.ForeignKey('self', null=True, on_delete=models.CASCADE)
-    type = models.CharField(choices=const.IMPORT_UNIT_TYPE_CHOICES),
+    type = models.CharField(
+        choices=const.IMPORT_UNIT_TYPE_CHOICES, 
+        max_length=const.IMPORT_UNIT_NAME_MAX_LENGTH
+    )
     price = models.PositiveBigIntegerField(blank=True, null=True)
