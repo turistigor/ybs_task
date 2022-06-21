40b54280399bbf4f00fc1596053798022309c9a8
igor <igor_m_87@mail.ru>
2022-06-21 15:13:02 +0300

    model and migration were created table for the import data

--- prices_comparator/models.py
+++ prices_comparator/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+import prices_comparator.common as const
+
+
+class ImportModel(models.Model):
+    id = models.UUIDField(primary_key=True)
+    name = models.CharField(max_length=const.IMPORT_UNIT_NAME_MAX_LENGTH)
+    date = models.DateTimeField()
+    parent_id = models.ForeignKey('self', null=True, on_delete=models.CASCADE)
+    type = models.CharField(choices=const.IMPORT_UNIT_TYPE_CHOICES),
+    price = models.PositiveBigIntegerField(blank=True, null=True)
